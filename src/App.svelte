021c23e443b9cde78c272407af2effdb001e3cb4
GCastilho <castilho.gabriel14@gmail.com>
2020-08-07 09:22:14 -0300

    Implementado transição entre os slides
    -- Implementado uma animação de transição 'slide to the side' entre os slides

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { fade } from 'svelte/transition'
+	import { onMount } from 'svelte'
 	import * as slide from './stores/slide'
 	import BoasVindas from './slides/SeletorLocal/index.svelte'
 	import SeletorConcessionaria from './slides/SeletorConcessionaria.svelte'
@@ -7,11 +7,7 @@
 	import FormDadosCliente from './slides/FormDadosCliente.svelte'
 	import Resultado from './slides/Resultado.svelte'
 
-	let fowardable = false
-	slide.subscribe(v => {
-		fowardable = slide.getSlidesVisited() > v
-	})
-
+	let div // Referência a div wrapper do slideshow
 	const slides = [
 		BoasVindas,
 		SeletorConcessionaria,
@@ -19,11 +15,62 @@
 		FormDadosCliente,
 		Resultado
 	]
+
+	/**
+	 * Resolve depois de um tempo suficiente para o elemento ter sua posição
+	 * atualizada
+	 */
+	function cssUpdateTime() {
+		return new Promise(resolve => setTimeout(resolve, 10))
+	}
+
+	/** Resolve quando o evento 'transitionend' da div for emitido */
+	function transitionend() {
+		return new Promise(resolve => {
+			function resolveAndCleanup() {
+				div.removeEventListener('transitionend', resolveAndCleanup)
+				resolve()
+			}
+			div.addEventListener('transitionend', resolveAndCleanup)
+		})
+	}
+
+	let slidable = true
+	async function slideToTheSide(side) {
+		const percentage = side == 'left' ? -110 : 110
+		div.style.left = percentage + '%'
+		await transitionend()
+		slidable = false
+		div.style.left = -1 * percentage + '%'
+		await cssUpdateTime()
+		slidable = true
+	}
+
+	let fowardable = false
+	let slideCounter = $slide
+	onMount(() => {
+		slide.subscribe(async v => {
+			fowardable = slide.getSlidesVisited() > v
+			if (v > slideCounter) {
+				await slideToTheSide('left')
+			} else if (v < slideCounter) {
+				await slideToTheSide('right')
+			}
+			slideCounter = v
+			div.style.left = null
+		})
+	})
 </script>
 
 <style>
 	div {
 		text-align: center;
+		position: relative;
+		left: 0;
+	}
+
+	.slidable {
+		transition: left 0.25s;
 	}
 
 	button {
@@ -39,7 +86,7 @@
 </style>
 
 <button on:click="{slide.previous}" class:hidden={!$slide}>&lt;</button>
-<div>
-	<svelte:component this={slides[$slide]} />
+<div bind:this={div} class:slidable>
+	<svelte:component this={slides[slideCounter]} />
 </div>
 <button on:click="{slide.next}" disabled={!fowardable}>&gt;</button>
